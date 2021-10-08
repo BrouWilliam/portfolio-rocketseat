d576113b813208abd3afd4ef619c0256e8070e79
BrouWilliam <brouwilliam@hotmail.com>
2021-10-08 13:52:17 -0300

    update backend page about

--- server.js
+++ server.js
@@ -9,11 +9,31 @@ server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get('/', function(req, res){
-    return res.render('about')
+    const about = {
+        avatarUrl: 'https://avatars.githubusercontent.com/u/36682030?v=4',
+        name: 'William Pereira',
+        role: 'Desenvolvedor Front-end',
+        description: 'Programador full-stack, focando em trazer soluções de tecnologia para o mercado',
+        links: [
+            {
+                name: 'Github', url: 'https://github.com/BrouWilliam',
+            },
+            {
+                name: 'Twitter', url: '',
+            },
+            {
+                name: 'Linkedin', url: 'https://www.linkedin.com/in/william-xavier-pereira-06927915b'
+            }
+        ]
+
+    }
+    
+    return res.render('about', { about })
 })
 
 server.get('/portfolio', function(req, res){
